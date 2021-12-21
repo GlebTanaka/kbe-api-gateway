5300db8695b6e3b4b81389dd3db6bc1342409b83
User5867 <lindi.f01@gmail.com>
2021-12-21 16:08:00 +0100

    add Gateway Service

---
+++ src/main/java/de/htwberlin/f4/apigateway/services/GatewayService.java
@@ -0,0 +1,33 @@
+package de.htwberlin.f4.apigateway.services;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class GatewayService {
+    public Double getMehrwertsteuer(double price){
+        final String uri = "http://localhost:8081/api/v1/calculator/calculatemehrwertsteuer?preis=" + price;
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(uri, Double.class);
+    }
+
+    public Object getProducts(){
+        final String uri = "http://localhost:8080/api/v1/product";
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(uri, Object.class);
+    }
+
+    public Object getProduct(UUID id){
+        final String uri = "http://localhost:8080/api/v1/product/" + id;
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(uri, Object.class);
+    }
+
+    public void exportProduct(){
+        final String uri = "http://localhost:8080/api/v1/product/export";
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject(uri, Object.class);
+    }
+}
